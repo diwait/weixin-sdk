7108043c02f2d29f3677f60ecfca3175770e8217
zdw <1368074250@qq.com>
2016-01-30 00:13:15 +0800

    edit composer.json

---
+++ examples/get_access_token.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * 文件 : get_access_token.php
+ * 作者 : David
+ * 日期 : 2016/01/30
+ * 备注 : 无
+ */
---
+++ src/curl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 文件 : curl.php
+ * 作者 : David
+ * 日期 : 2016/01/30
+ * 备注 : 无
+ */
+namespace diwait\weixin_sdk\src;
+
+class curl {
+
+    public function test() {
+        echo 'hello world!';
+    }
+
+}
